e8532325bdd30b8619b883641af94fdfa6c11611
Tjin Au Yeung <tjinauyeung@gmail.com>
2016-02-23 11:17:17 +0100

    fixed orginasation

--- src/App.js
+++ src/App.js
@@ -1,50 +1,17 @@
 import React from 'react';
-import ListInput from './listInput';
-import ListItem from './listItem';
+import ToDoList from './ToDoList.js';
 
 
 class App extends React.Component {
 
 	constructor(){
 		super();
-		this.state = {
-			items: ["Call my mom", "Do the groceries", "Find my cat", "Finish my homework"]
-		};
-	}
-
-	onChangeDestroy(item){
-
-		this.state.items.splice(item, 1);
-
-		this.setState({
-			items: this.state.items
-		});
-	}
-
-	onMarkDone(index){
-		var items = this.state.items;
-   		var item = this.state.items[index];
-   		console.log(item);
-	}
-
-	renderItems(item){
-		return <ListItem itemname={item} onChange={this.onChangeDestroy.bind(this)} onMark={this.onMarkDone.bind(this)}/>;
-	}
-
-	onAddListItem(item) {
-		var newItem = item;
-		var newItems = this.state.items.concat(newItem);
-		this.setState({
-			items: newItems
-		});
 	}
 
 	render() {
         return (
         	<div class="container">
-            	<h1>My First To Do App</h1>
-            	<ListInput onSubmit={this.onAddListItem.bind(this)} />
-            	<div>{this.state.items.map(this.renderItems.bind(this))}</div>
+        		<ToDoList />
             </div>
         );
     }
--- src/toDoList.js
+++ src/toDoList.js
@@ -1,1 +1,52 @@
+import React from 'react';
+import ListInput from './listInput';
+import ListItem from './listItem';
 
+class ToDoList extends React.Component {
+
+	constructor(){
+		super();
+		this.state = {
+			items: ["Call my mom", "Do the groceries", "Find my cat", "Finish my homework"]
+		};
+	}
+
+	onChangeDestroy(item){
+
+		this.state.items.splice(item, 1);
+
+		this.setState({
+			items: this.state.items
+		});
+	}
+
+	onMarkDone(index){
+		var items = this.state.items;
+   		var item = this.state.items[index];
+   		console.log(item);
+	}
+
+	renderItems(item){
+		return <ListItem itemname={item} onChange={this.onChangeDestroy.bind(this)} onMark={this.onMarkDone.bind(this)}/>;
+	}
+
+	onAddListItem(item) {
+		var newItem = item;
+		var newItems = this.state.items.concat(newItem);
+		this.setState({
+			items: newItems
+		});
+	}
+
+	render() {
+        return (
+        	<div>
+        	<h1>My First To Do App</h1>
+            	<ListInput onSubmit={this.onAddListItem.bind(this)} />
+            	<div>{this.state.items.map(this.renderItems.bind(this))}</div>
+            </div>
+        );
+	}
+}
+
+export default ToDoList;
